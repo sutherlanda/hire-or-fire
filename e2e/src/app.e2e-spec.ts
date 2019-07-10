922ee700a188e4d701561eab070f82289bdd41b0
Andrew Sutherland <sutherlanda@gmail.com>
2019-07-09 22:10:30 -0700

    Update single e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to hire-or-fire!');
+    expect(page.getTitleText()).toEqual('Hire-or-Fire?');
   });
 
   afterEach(async () => {
